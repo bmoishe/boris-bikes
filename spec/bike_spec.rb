aeca4ed60bfb912e5debe40cdffd51a6bc6b82c0
bmoishe <moishebarnes@gmail.com>
2018-10-04 13:44:07 +0100

    step 10

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,9 @@
 require 'bike.rb'
 
+#    As a person,
+#    So that I can use a good bike,
+#    I'd like to see if a bike is working
+
 describe Bike do
   it { is_expected.to respond_to :working? }
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,18 @@ require 'docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+#  As a person,
+#  So that I can use a bike,
+#  I'd like to get a bike from a docking station.
+
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
+
+#    As a person,
+#    So that I can use a good bike,
+#    I'd like to see if a bike is working
+
   end
 end
